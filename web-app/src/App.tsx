8d6d5cda7b1c7c79eb45578320ce771df6b012c2
Piotr 'Ugz' Bielski <piotr.bielski@polishdev.com>
2024-03-06 00:39:58 +0100

    refactored App component in template

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+export const App = () => {
   const [count, setCount] = useState(0)
 
   return (
@@ -30,6 +30,4 @@ function App() {
       </p>
     </>
   )
-}
-
-export default App
+};
