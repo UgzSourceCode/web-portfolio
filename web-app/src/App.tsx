cd4b76dcdedd926aa33f640fe629b00c239f2ff9
Piotr 'Ugz' Bielski <piotr.bielski@polishdev.com>
2024-03-09 13:51:44 +0100

    added exceptions for linter

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -9,16 +9,16 @@ export const App = () => {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button type="button" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
--- web-app/src/main.tsx
+++ web-app/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 import './index.css'
 
+// biome-ignore lint: lint/style/noNonNullAssertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
