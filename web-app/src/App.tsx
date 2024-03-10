23967f33985b191c2b88d958aad89bb1d0fed130
Piotr 'Ugz' Bielski <piotr.bielski@polishdev.com>
2024-03-10 20:52:15 +0100

    fixed not fixable issues from linter

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -18,7 +18,7 @@ export const App = () => {
 			</div>
 			<h1>Vite + React</h1>
 			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
+				<button type="button" onClick={() => setCount((count) => count + 1)}>
 					count is {count}
 				</button>
 				<p>
--- web-app/src/main.tsx
+++ web-app/src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-);
+const rootContainer = document.getElementById("root");
+if (rootContainer) {
+	ReactDOM.createRoot(rootContainer).render(
+		<React.StrictMode>
+			<App/>
+		</React.StrictMode>,
+	);
+}
