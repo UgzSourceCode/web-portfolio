dd6e5fd0a723e8bee61de8c692e29410069abf80
Piotr 'Ugz' Bielski <piotr.bielski@polishdev.com>
2024-03-10 20:53:02 +0100

    fixed lint issues

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -9,10 +9,10 @@ export const App = () => {
 	return (
 		<>
 			<div>
-				<a href="https://vitejs.dev" target="_blank">
+				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
 					<img src={viteLogo} className="logo" alt="Vite logo" />
 				</a>
-				<a href="https://react.dev" target="_blank">
+				<a href="https://react.dev" target="_blank" rel="noreferrer">
 					<img src={reactLogo} className="logo react" alt="React logo" />
 				</a>
 			</div>
