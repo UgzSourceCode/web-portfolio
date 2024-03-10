648bcd1bbeaf1758cf1ffe19d3d6d7ea6408f906
Piotr 'Ugz' Bielski <piotr.bielski@polishdev.com>
2024-03-10 14:54:57 +0100

    fixed code before tests

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+// eslint-disable-next-line import/no-unresolved
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -9,10 +10,10 @@ export const App = () => {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
