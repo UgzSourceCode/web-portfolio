b4a80169ab2bd89a34aed2e39ce134203e78d1e0
Piotr 'Ugz' Bielski <piotr.bielski@polishdev.com>
2024-03-14 00:05:01 +0100

    added config for i18next

--- web-app/src/main.tsx
+++ web-app/src/main.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, {Suspense} from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./theme";
 
+import "./i18n.ts";
+
 const rootContainer = document.getElementById("root");
 if (rootContainer) {
 	ReactDOM.createRoot(rootContainer).render(
 		<React.StrictMode>
 			<ChakraProvider theme={theme}>
-				<App />
+				<Suspense fallback="Loading...">
+					<App />
+				</Suspense>
 			</ChakraProvider>
 		</React.StrictMode>,
 	);
